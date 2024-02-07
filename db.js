323b6a5c03cd241a048c11532b65bf29a8b1d056
Olga <888yalosinger@ukr.net>
2024-02-07 11:44:49 -0500

    initial commit

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const DB_URL =
-    // process.env.NODE_ENV === 'development'
-        // ? 'mongodb://127.0.0.1:27017/todolist'
-         'mongodb+srv://olgaorlova241:druYgJ4o6vMYg4Q7@cluster0.e3sqktx.mongodb.net/todolist?retryWrites=true&w=majority';
+    process.env.NODE_ENV === 'development'
+        ? 'mongodb://127.0.0.1:27017/todolist'
+        : 'mongodb+srv://olgaorlova241:druYgJ4o6vMYg4Q7@cluster0.e3sqktx.mongodb.net/todolist?retryWrites=true&w=majority';
 
 (async () => {
     await mongoose.connect(DB_URL);
